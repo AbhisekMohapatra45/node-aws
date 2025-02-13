583d8872dc1a4040f5a7acaef6e81d7fbaa68356
AbhisekMohapatra45 <abhisekmohapatra45@gmail.com>
2025-02-13 13:20:36 +0530

    port change

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ import { app } from "./index.js";
 
 const server = createServer(app);
 
-server.listen(4488, () => {
-  console.log("Server is running on port 4488");
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
