66ee8d43c079a2dc0400f34ed131ae2d391d2ff9
AbhisekMohapatra45 <abhisekmohapatra45@gmail.com>
2025-02-18 11:28:09 +0530

    minor changes done

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
+  const { query: { name } = {} } = req;
+  if (name) {
+    return res.send(`hello ${name}`);
+  }
   res.send("hello world");
 });
 
